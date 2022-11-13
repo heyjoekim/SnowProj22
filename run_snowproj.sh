ce6a304366e8a2a77845d14b34ca7aab878a33ea
Haejo Kim <112507232+heyjoekim@users.noreply.github.com>
2022-11-13 13:56:14 -0500

    Update run_snowproj.sh

--- run_snowproj.sh
+++ run_snowproj.sh
@@ -6,7 +6,7 @@ echo "obtaining Data from NSIDC"
 chmod +x ./code/nsidc_download.sh
 chmod +x ./code/nsidc_download_win.sh
 # execute nsidc download script
-if [$1 -eq "windows"]
+if [ $1="windows" ]
 then
   sh ./code/nsidc_download_win.sh $2 $3
 else
